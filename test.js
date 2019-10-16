5775e48895e3541e7d6625158d86a562b316543c
Poly Goveia <poly.goveia@gmail.com>
2019-10-15 19:38:01 -0700

    write tests

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
 const wordPattern = require("./index");
+
+test("Word Pattern is a function", () => {
+  expect(typeof wordPattern).toEqual("function");
+});
+
+test('pattern = "abba", str = "dog cat cat dog" should return truth', () => {
+  expect(wordPattern("abba", "dog cat cat dog")).toBeTruthy();
+});
+
+test('pattern = "aaaa", str = "dog dog dog dog" should return truth', () => {
+  expect(wordPattern("aaaa", "dog dog dog dog")).toBeTruthy();
+});
+
+test('pattern = "aaaa", str = "dog dog cat cat" should return truth', () => {
+  expect(wordPattern("aabb", "dog dog cat cat")).toBeTruthy();
+});
+
+test('pattern = "abba", str = "dog cat cat fish" should return false', () => {
+  expect(wordPattern("abba", "dog cat cat fish")).toBeFalsy();
+});
+
+test('pattern = "aaaa", str = "dog cat cat dog" should return false', () => {
+  expect(wordPattern("aaaa", "dog cat cat dog")).toBeFalsy();
+});
+
+test('pattern = "abba", str = "dog dog dog dog" should return false', () => {
+  expect(wordPattern("abba", "dog dog dog dog")).toBeFalsy();
+});
